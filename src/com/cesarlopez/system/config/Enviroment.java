d12c570c487910cdb4b08de2629efe6dca3aa071
Cesar Lopez <clopez-2026231@kinal.edu.gt>
2026-08-19 17:06:03 -0600

    feat: Se elimino el proyecto restante

---
+++ src/com/cesarlopez/system/config/Enviroment.java
@@ -0,0 +1,9 @@
+package com.cesarlopez.system.config;
+
+public class Enviroment {
+    
+   protected final static String USER = "IN4AV";
+    protected static final String PASSWORD = "&mnid4AV";
+    protected static final String DATA_BASE = "auditoria_usuario_producto_in4av";
+    protected static final String LOCATION_SERVICE = "localhost: 3306";
+}
